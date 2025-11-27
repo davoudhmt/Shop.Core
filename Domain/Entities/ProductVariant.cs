30932c96096b8006cb5e6999c5281157f29f3e0b
davoudhmt <davoud.hmt@gmail.com>
2025-11-28 00:57:38 +0330

    feat: add productvariant entity edit product, productfeature, productfaeaturevalue, productimage

---
+++ Domain/Entities/ProductVariant.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductVariant : BaseEntity
+    {
+        /// <summary>
+        /// مقدار شناسه محصول
+        /// </summary>
+        public long ProductId { get; set; }
+
+        /// <summary>
+        /// مقدار قیمت محصول
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// مقدار موجودی محصول
+        /// </summary>
+        public int Stock { get; set; }
+
+        /// <summary>
+        /// مقدار کد SKU محصول
+        /// </summary>
+        public string SKU { get; set; } = string.Empty;
+
+        /// <summary>
+        /// مجموعه مقادیر ویژگی‌های محصول
+        /// </summary>
+        public ICollection<ProductFeatureValue> VariantFeatures { get; set; } = new List<ProductFeatureValue>();
+
+        /// <summary>
+        /// مجموعه تصاویر محصول
+        /// </summary>
+        public ICollection<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
+    }
+}
