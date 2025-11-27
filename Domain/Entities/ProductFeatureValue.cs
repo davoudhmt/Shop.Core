21cb250145131f468335d9ed7e436ec2d49baf13
davoudhmt <davoud.hmt@gmail.com>
2025-11-27 22:21:35 +0330

    feat: add productfeaturevalue entity

---
+++ Domain/Entities/ProductFeatureValue.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductFeatureValue : BaseEntity
+    {
+        /// <summary>
+        /// مقدار شناسه محصول
+        /// </summary>
+        public long ProductId { get; set; }
+
+        /// <summary>
+        /// مقدار شناسه ویژگی محصول
+        /// </summary>
+        public long ProductFeatureId { get; set; }
+
+        /// <summary>
+        /// مقدار ویژگی محصول
+        /// </summary>
+        public string Value { get; set; } = string.Empty;
+    }
+}
